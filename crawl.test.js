5344343a8f5bd6a45d190899229421a3672dd694
GoldenMM <ryanjanderton@yahoo.com.au>
2024-08-08 12:54:36 +0800

    made the testcase structure more verbose

--- crawl.test.js
+++ crawl.test.js
@@ -1,18 +1,28 @@
 import {test, expect} from '@jest/globals'
 import {normalizeURL} from './crawl.js'
 
-const expectedURL = 'blog.boot.dev/path'
-
+//normalizeURL Testing
 const normalizeURL_testCases = [
-    'https://blog.boot.dev/path/',
-    'https://blog.boot.dev/path',
-    'http://blog.boot.dev/path/',
-    'http://blog.boot.dev/path',
-    'http://blog.BoOt.dev/Path'
+    {   input: 'https://blog.boot.dev/path/',
+        output: 'blog.boot.dev/path',
+        testType: 'trailing slash'
+    },
+    {   input: 'https://blog.boot.dev/path',
+        output: 'blog.boot.dev/path',
+        testType: 'no trailing slash'
+    },
+    {   input: 'http://blog.boot.dev/path',
+        output: 'blog.boot.dev/path',
+        testType: 'protocol independent'
+    },
+    {   input: 'http://blog.BoOt.dev/Path',
+        output: 'blog.boot.dev/path',
+        testType: 'case insensitive'
+    }
 ]
 
-for (const url of normalizeURL_testCases) {
-    test(`normalizeURL (${url})`, () => {
-        expect(normalizeURL(url)).toBe(expectedURL)
+for (const testCase of normalizeURL_testCases) {
+    test(`normalizeURL (${testCase.testType})`, () => {
+        expect(normalizeURL(testCase.input)).toBe(testCase.output)
     })
 }
